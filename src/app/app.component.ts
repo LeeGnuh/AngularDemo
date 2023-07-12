51653d3d449603117ec587a4b930c52f3ab2c78c
Your Name <you@example.com>
2023-07-12 17:00:45 +0700

    12/7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    fontSizePx = 16;
+
+  /*-------------------------Data binding-------------------------*/
+  //One-way
+  
+  bool: boolean = true;
+  turn() {this.bool=!this.bool;}
+
+  fontSizePx = 20;
+  
+  size: number | string = this.fontSizePx;
+  sizeChange = new EventEmitter<number>();
+  
+  dec() { this.resize(-1); }
+  inc() { this.resize(+1); }
+  
+  resize(delta: number) {
+    this.size = Math.min(40, Math.max(8, +this.size + delta));
+    this.sizeChange.emit(this.size);
+    this.fontSizePx = this.size;
+  }
+
+  //Two-way
+  something:string="";
+
+  /*------------------Template Reference Variable-----------------*/
+
+  callPhone(phone: string): void {
+    this.something=phone;
+  }
+//ngForm
+  submitMessage: string = 'Value input valid!';
+
+  onSubmit(itemForm: { value: { name: any; email: any; }; }): void {
+    console.log(itemForm.value.name);
+    console.log(itemForm.value.email);
+  }
 }
